b09fa2200e2e4e07c1697620f674496f408d354a
Poobharathii <Poobharathii@ips-securex.com>
2023-03-01 11:30:12 +0800

    first commit

---
+++ Test.py
@@ -0,0 +1,24 @@
+a=0
+b=5
+x=(a&b)|(a&a)|(a|b)
+print(x)
+
+str1="Programming Language"
+print(str1.find("m"))
+
+42//2
+21%6
+12/4
+11*2 
+
+
+def loopLetters(words):
+    x = []
+    for ch in words:
+        x = x + [ch]
+    return x
+print(loopLetters("Hello"))
+
+
+
+_str1="Mary,Harry,John,Sam"
